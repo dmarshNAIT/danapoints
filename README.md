2c9990c5034ee01534437738e7d47457583871f2
BurlierEarth7 <j.tollefson@outlook.com>
2024-11-16 17:10:09 -0700

    Work on styles & fix .md formatting
    Added content to styles, added reset sheet to index.html
    
    Fixed README.md and TODO.md to properly reflect markdown syntax
    
    PHP is currently a test, and may be replaced later with another language

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# danapoints
 
+# Dana Points
 
+## Contributors
 
-### Contributors
 - [BurlierEarth7](https://github.com/BurlierEarth7)
 
+## Tasks
 
-### Tasks
 - [A project](https://github.com/users/dmarshNAIT/projects/1) has been created to track to-do items.
--- php/database.php
+++ php/database.php
@@ -4,21 +4,26 @@ namespace Database;
 class Database
 {
     // WHY IS IT DIFFERENT FROM MYSQL!?
-    private static $servername = "serverName\\sqlexpress";
+    private static $servername = "BURLIEREARTH7-M\\SQLEXPRESS";
     private static $connInfo = array(
-        "Database" => "dbName",
-        "UID" => "username",
-        "PWD" => "password",
+        "Database" => "Danabase",
+        "UID" => "Tollefson",
+        "PWD" => "",
     );
 
-    private static $conn = null;
 
-    public static function connect($servername, $connInfo)
+    function __construct($servername, $connInfo)
     {
-        self::$conn = sqlsrv_connect($servername, $connInfo);
-
-        if (self::$conn) {
-            die("" . self::$conn);
+        // Connect to MS SQL database
+        $conn = sqlsrv_connect($servername, $connInfo);
+        if (!$conn) {
+            die(print_r(sqlsrv_errors(), true));
+        } else {
+            print ("Connected!");
         }
     }
 }
+
+$connection = new Database("BURLIEREARTH7-M\\SQLEXPRESS", array(
+    "Database" => "Danabase"
+));
